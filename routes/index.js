1a8e1de02206eb6ecd31d13649a2d787598b1602
Jay Hurst <jhurst@salesforce.com>
2016-09-18 23:46:11 -0700

    Change from Form to AJAX Post 1

--- routes/index.js
+++ routes/index.js
@@ -21,16 +21,8 @@ router.post('/', function(req, res, next) {
 	newEvent.set('Product_Name__c', req.body.productName);
 	newEvent.set('Number_of_Units__c', req.body.numberOfUnits);
 	newEvent.set('Total_Amount__c', req.body.totalAmount);  
-console.log('newEvent: ' + req.body);
 
-	// var newEvent = nforce.createSObject('Order_Response__e');
-// 	newEvent.set();  newEvent.set('Account_Executive__c', 'Test');
-// 	newEvent.set('Contact_by_Date__c', '2016-01-01');
-// 	newEvent.set('Email_Address__c', 'test@test.com');
-// 	newEvent.set('Phone_Number__c', '555-5555');
-  
-
-	org.org.insert({ sobject: newEvent });
+	org.org.insert({ sobject: newEvent })
   
 });
 
