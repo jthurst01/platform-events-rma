88e7b7b37ab4fea06adb16763bf0c8263a15a319
Jay Hurst <jhurst@salesforce.com>
2016-10-26 13:21:03 -0700

    Add Replay Functionality 4

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ exports.DEBUG = true;
 exports.ENVIRONMENT = 'production';
 exports.CALLBACK_URL = 'http://localhost:3001';
 exports.TOPIC = '/event/Return_Status__e';
-exports.REPLAY_ID = -2;
+exports.REPLAY_ID = 269;
 
 exports.CLIENT_ID = "3MVG9SemV5D80oBcjsq97hHAUFcvbzU68h8Qd3DoYvB4fHUxGviPd57zmDEzTAFEsbaQUGyY1OOxJ_ACNMv5s";
 exports.CLIENT_SECRET = "8410631505093561599";
