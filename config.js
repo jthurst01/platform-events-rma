86a1e1c8ad2b813cf3fc6d528cb1eb5349bd696e
Jay Hurst <jhurst@salesforce.com>
2017-01-30 22:19:20 -0800

    add blitz

--- config.js
+++ config.js
@@ -1,11 +1,16 @@
 exports.PORT = process.env.PORT || 3001; // use heroku's dynamic port or 3001 if localhost
 exports.DEBUG = true;
-exports.ENVIRONMENT = 'production';
+exports.ENVIRONMENT = 'blitz';
 exports.CALLBACK_URL = 'http://localhost:3001';
 exports.TOPIC = '/event/Return_Status__e';
 exports.REPLAY_ID = -2;
 
-exports.CLIENT_ID = "3MVG9SemV5D80oBcjsq97hHAUFcvbzU68h8Qd3DoYvB4fHUxGviPd57zmDEzTAFEsbaQUGyY1OOxJ_ACNMv5s";
-exports.CLIENT_SECRET = "8410631505093561599";
-exports.USERNAME = "admin@acmeservice.com";
+//exports.CLIENT_ID = "3MVG9SemV5D80oBcjsq97hHAUFcvbzU68h8Qd3DoYvB4fHUxGviPd57zmDEzTAFEsbaQUGyY1OOxJ_ACNMv5s";
+//exports.CLIENT_SECRET = "8410631505093561599";
+//exports.USERNAME = "admin@acmesandbox.com";
+//exports.PASSWORD = "test1234";
+
+exports.CLIENT_ID = "3MVG9Iu66FKeHhIOe19VLgc65L.ZruC5vl7ZMHWuTfAFOaE6beZRU6KrlxbUxLzkj8mKjGx2H40QBj.CO1AsF";
+exports.CLIENT_SECRET = "6467468359680911869";
+exports.USERNAME = "admin@acme208.com";
 exports.PASSWORD = "test1234";
