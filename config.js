949895b1b4f08a2778aca8dbf4a6c927a9b8d6bb
Jay Hurst <jhurst@salesforce.com>
2017-04-03 17:55:57 -0700

    Remove Bluebird 2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.PORT = process.env.PORT || 3001; // use heroku's dynamic port or 3001 if localhost
 exports.DEBUG = true;
-exports.ENVIRONMENT = 'blitz';
+exports.ENVIRONMENT = 'production';
 exports.CALLBACK_URL = 'http://localhost:3001';
 exports.TOPIC = '/event/Return_Status__e';
 exports.REPLAY_ID = -1;
