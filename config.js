f32ad76b9f967a119776860a4c3471d329b426b9
Jay Hurst <jhurst@salesforce.com>
2017-04-06 15:01:32 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,7 @@ exports.CALLBACK_URL = 'http://localhost:3001';
 exports.TOPIC = '/event/Return_Status__e';
 exports.REPLAY_ID = -1;
 
-exports.CLIENT_ID = "3MVG9SemV5D80oBcjsq97hHAUFcvbzU68h8Qd3DoYvB4fHUxGviPd57zmDEzTAFEsbaQUGyY1OOxJ_ACNMv5s";
-exports.CLIENT_SECRET = "8410631505093561599";
-exports.USERNAME = "admin@acmeservice.com";
-exports.PASSWORD = "test1234";
-
-//exports.CLIENT_ID = "3MVG9Iu66FKeHhIOe19VLgc65L.ZruC5vl7ZMHWuTfAFOaE6beZRU6KrlxbUxLzkj8mKjGx2H40QBj.CO1AsF";
-//exports.CLIENT_SECRET = "6467468359680911869";
-//exports.USERNAME = "admin@acme208.com";
-//exports.PASSWORD = "test1234";
+exports.CLIENT_ID = "";
+exports.CLIENT_SECRET = "";
+exports.USERNAME = "";
+exports.PASSWORD = "";
