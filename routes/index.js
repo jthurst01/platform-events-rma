8dad74e3ef7272e7c2256c3b01b96ed3e7be3bb9
Jay Hurst <jhurst@salesforce.com>
2017-05-26 09:04:19 -0700

    Update for Package

--- routes/index.js
+++ routes/index.js
@@ -19,23 +19,28 @@ router.get('/cdc', function(req, res, next) {
 /* Creates a new the record */
 router.post('/', function(req, res) {
 	console.log('In router post');
-	var newEvent = nforce.createSObject('RMA_Request__e');
-	newEvent.set('AccountId__c', req.body.accountId);
-	newEvent.set('Asset_Type__c', req.body.assetType);
-	newEvent.set('Serial_Number__c', req.body.serialNumber);
-	newEvent.set('Issue_Date__c', req.body.issueDate);
-	newEvent.set('Issue_Description__c', req.body.issueDescription);  
+	var newEvent = nforce.createSObject('acme_services__RMA_Request__e');
+	newEvent.set('acme_services__AccountId__c', req.body.accountId);
+	newEvent.set('acme_services__Asset_Type__c', req.body.assetType);
+	newEvent.set('acme_services__Serial_Number__c', req.body.serialNumber);
+	newEvent.set('acme_services__Issue_Date__c', req.body.issueDate);
+	newEvent.set('acme_services__Issue_Description__c', req.body.issueDescription);  
 	app.org.insert({ sobject: newEvent })
 	res.sendStatus(200);
 });
 
 router.post('/newSub', function(req, res) {
 	var cj = app.org.createStreamClient({ topic: app.config.TOPIC, replayId: app.config.REPLAY_ID });
-	var str = cj.subscribe({ topic: app.config.TOPIC, oauth: app.oauth });
+	var str = cj.disconnect({ topic: app.config.TOPIC, oauth: app.oauth });
+	str = cj.subscribe({ topic: app.config.TOPIC, oauth: app.oauth });
 	//console.log(util.inspect(cj, false, null));
 
 	str.on('connect', function(){
 		console.log('Connected to topic: ' + app.config.TOPIC);
+	});
+	
+	str.on('disconnect', function(){
+		console.log('Disconnected from topic: ' + app.config.TOPIC);
 	});
 
 	str.on('error', function(error) {
@@ -52,12 +57,13 @@ router.post('/newSub', function(req, res) {
 });
 
 router.post('/newCDCSub', function(req, res) {
-	var cj = app.org.createStreamClient({ topic: '/data/LeadChangeManualEvent', replayId: -2 });
-	var str = cj.subscribe({ topic: '/data/LeadChangeManualEvent', oauth: app.oauth });
+	var cj = app.org.createStreamClient({ topic: '/data/ChangeEvents', replayId: -2 });
+	var str = cj.disconnect({ topic: app.config.TOPIC, oauth: app.oauth });
+	str = cj.subscribe({ topic: '/data/ChangeEvents', oauth: app.oauth });
 	//console.log(util.inspect(cj, false, null));
 
 	str.on('connect', function(){
-		console.log('Connected to topic: ' + '/data/LeadChangeManualEvent');
+		console.log('Connected to topic: ' + '/data/ChangeEvents');
 	});
 
 	str.on('error', function(error) {
